e0cbc8c3595cef61f5a3fe784d38541c0a96cd0a
RuthTubauG <ruthtubaugonzalez@gmail.com>
2024-01-24 20:12:00 +0100

    probando

--- src/app/pages/add-beer/add-beer.page.ts
+++ src/app/pages/add-beer/add-beer.page.ts
@@ -2,18 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { menuOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { IonButtons, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-add-beer',
   templateUrl: './add-beer.page.html',
   styleUrls: ['./add-beer.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, IonButtons, IonButton]
 })
 export class AddBeerPage implements OnInit {
   volume: string = 'cl';
 
-  constructor() { }
+  constructor() { 
+    addIcons({
+      menuOutline
+    });
+  }
 
   ngOnInit() {
   }
--- src/app/pages/beer-catalog/beer-catalog.page.ts
+++ src/app/pages/beer-catalog/beer-catalog.page.ts
@@ -2,17 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { menuOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { IonButtons, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-beer-catalog',
   templateUrl: './beer-catalog.page.html',
   styleUrls: ['./beer-catalog.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, IonButtons, IonButton]
 })
 export class BeerCatalogPage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({
+      menuOutline
+    });
+  }
 
   ngOnInit() {
   }
