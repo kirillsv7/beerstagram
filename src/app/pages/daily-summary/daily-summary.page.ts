d9fa7e060b9121bd56c8cec8d2dc015ee0c4f5d2
RuthTubauG <ruthtubaugonzalez@gmail.com>
2024-01-25 16:25:04 +0100

    footer/titulos y iconos

--- src/app/pages/daily-summary/daily-summary.page.ts
+++ src/app/pages/daily-summary/daily-summary.page.ts
@@ -2,17 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { menuOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { IonButtons, IonButton, IonMenu } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-daily-summary',
   templateUrl: './daily-summary.page.html',
   styleUrls: ['./daily-summary.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, IonButtons, IonButton, IonMenu]
 })
-export class DailySummaryPage  {
+export class DailySummaryPage implements OnInit { 
 
   constructor() {
+    addIcons({
+      menuOutline
+    });
+  }
+
+  ngOnInit() {
   }
 
 }
--- src/app/pages/stats/stats.page.ts
+++ src/app/pages/stats/stats.page.ts
@@ -2,17 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { menuOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { IonButtons, IonButton, IonMenu } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.page.html',
   styleUrls: ['./stats.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, IonButtons, IonButton, IonMenu]
 })
 export class StatsPage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({
+      menuOutline
+    });
+  }
 
   ngOnInit() {
   }
