3076879cf336f4d6b3df23945679546c8b49b132
RuthTubauG <ruthtubaugonzalez@gmail.com>
2024-01-25 18:34:42 +0100

    funcional

--- src/app/pages/daily-summary/daily-summary.page.ts
+++ src/app/pages/daily-summary/daily-summary.page.ts
@@ -2,26 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { menuOutline } from 'ionicons/icons';
-import { addIcons } from 'ionicons';
-import { IonButtons, IonButton, IonMenu } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-daily-summary',
   templateUrl: './daily-summary.page.html',
   styleUrls: ['./daily-summary.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, IonButtons, IonButton, IonMenu]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
-export class DailySummaryPage implements OnInit { 
+export class DailySummaryPage  {
 
   constructor() {
-    addIcons({
-      menuOutline
-    });
-  }
-
-  ngOnInit() {
   }
 
 }
